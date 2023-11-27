68166a04a09cc7f29de71f1733e12fd4a1145e4c
kyle_lin <kyle_lin@baasid.com.tw>
2023-11-27 17:17:29 +0800

    feat: udemy basic react Lab

---
+++ src/components/video_list.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import VideoListItem from './video_list_item';
+
+const ViedoList = ({ videos, onVideoSelect }) => {
+    const videoItem = videos.map((video) => {
+        return (
+            <VideoListItem 
+                key={video.etag} 
+                video={video}
+                onVideoSelect={onVideoSelect}>
+            </VideoListItem>
+        );
+    });
+
+    return (
+        <ul className='col-md-4 list-group'>{ videoItem }</ul>
+    );
+};
+
+export default ViedoList;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+import _ from 'lodash';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
+import ViedoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyCNyDtHDrLYUjSiys5fISxkrpNGD1t-i_Q';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// create a new component. this component should produce some html
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { 
+            videos: [],
+            selectedVideo: null
+        };
+
+        this.videoSearch('羽毛球教學');
+    }
+
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term: term}, (videos) => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
+        });
+    }
+
+    render() {
+        const videoSearch = _.debounce((term) => { this.videoSearch(term)}, 300);
+
+        return ( 
+            <div>
+                <SearchBar onSearchTermChange={videoSearch}></SearchBar>
+                <VideoDetail video={this.state.selectedVideo}></VideoDetail>
+                <ViedoList 
+                    videos={this.state.videos}
+                    onVideoSelect={ selectedVideo => this.setState({selectedVideo}) }>
+                </ViedoList>
+            </div> 
+        )
+    }
+}
+
+// take this component′s generated html  and put it on the page (in the dom)
+ReactDOM.render(<App />, document.querySelector('.container'));
